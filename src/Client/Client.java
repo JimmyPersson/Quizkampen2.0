4faa2b8579cece9259ecf9fd85e223de3505024f
JimmyPersson <jimmy.persson@yh.nackademin.se>
2021-11-23 14:36:57 +0100

    Initial commit

---
+++ src/Client/Client.java
@@ -0,0 +1,103 @@
+package Client;
+
+import GUI.GamePanel;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import javax.swing.Icon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+
+public class Client {
+
+    private static int PORT = 8901;
+    private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
+
+    public Client(String serverAddress) throws Exception {
+        GamePanel test = new GamePanel();
+        socket = new Socket(serverAddress, PORT);
+        in = new BufferedReader(new InputStreamReader(
+                socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(), true);
+
+    }
+
+    /*public void play() throws Exception {
+        String response;
+        char mark = 'S';
+        char opponentMark = 'P';
+        try {
+            response = in.readLine();
+            if (response.startsWith("WELCOME")) {
+                mark = response.charAt(8);
+                opponentMark = (mark == 'X' ? 'O' : 'X');
+                frame.setTitle("Tic Tac Toe - Player " + mark);
+            }
+            while (true) {
+                response = in.readLine();
+                if (response.startsWith("VALID_MOVE")) {
+                    messageLabel.setText("Valid move, please wait");
+                    currentSquare.setText(String.valueOf(mark));
+                    currentSquare.repaint();
+                } else if (response.startsWith("OPPONENT_MOVED")) {
+                    int loc = Integer.parseInt(response.substring(15));
+                    board[loc].setText(String.valueOf(opponentMark));
+                    board[loc].repaint();
+                    messageLabel.setText("Opponent moved, your turn");
+                } else if (response.startsWith("VICTORY")) {
+                    messageLabel.setText("You win");
+                    break;
+                } else if (response.startsWith("DEFEAT")) {
+                    messageLabel.setText("You lose");
+                    break;
+                } else if (response.startsWith("TIE")) {
+                    messageLabel.setText("You tied");
+                    break;
+                } else if (response.startsWith("MESSAGE")) {
+                    messageLabel.setText(response.substring(8));
+                }
+            }
+            out.println("QUIT");
+        }
+        finally {
+            socket.close();
+        }
+    }*/
+
+    /*private boolean wantsToPlayAgain() {
+        int response = JOptionPane.showConfirmDialog(frame,
+                "Want to play again?",
+                "Tic Tac Toe is Fun Fun Fun",
+                JOptionPane.YES_NO_OPTION);
+        frame.dispose();
+        return response == JOptionPane.YES_OPTION;
+    }
+*/
+
+    public static void main(String[] args) throws Exception {
+        while (true) {
+            String serverAddress = (args.length == 0) ? "localhost" : args[1];
+            Client client = new Client(serverAddress);
+            client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            client.frame.setSize(240, 160);
+            client.frame.setVisible(true);
+            client.frame.setResizable(true);
+            client.play();
+            if (!client.wantsToPlayAgain()) {
+                break;
+            }
+        }
+    }
+}
---
+++ src/Server/Server.java
@@ -0,0 +1,30 @@
+package Server;
+
+import java.net.ServerSocket;
+
+
+public class Server {
+
+    public static void main(String[] args) throws Exception {
+        ServerSocket listener = new ServerSocket(8901);
+        System.out.println("Server has started");
+        try {
+            while (true) {
+
+                ServerSidePlayer player1
+                        = new ServerSidePlayer(listener.accept(), '1');
+                ServerSidePlayer player2
+                        = new ServerSidePlayer(listener.accept(), '2');
+                ServerSideGame game = new ServerSideGame(player1, player2);
+
+
+                player1.setOpponent(player2);
+                player2.setOpponent(player1);
+                game.currentPlayer = player1;
+                game.start();
+            }
+        } finally {
+            listener.close();
+        }
+    }
+}
